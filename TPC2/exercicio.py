6ac0846f8048889439a1477abe69c65ce9630438
JoseBambora <zebragapt@gmail.com>
2023-02-24 16:36:58 +0000

    Minor change

--- TPC2/exercicio.py
+++ TPC2/exercicio.py
@@ -23,7 +23,7 @@ def funaux(l):
     return list(filter(lambda str: str == '=',l[0])) + l[1:]
 
 def funtakeon(l):
-    return map(lambda str: funaux(str.split('on')),l[1:])
+    return map(lambda str: funaux(str.split('on')),l)
 
 def auxreduce(acc):
     print(acc)
@@ -32,7 +32,7 @@ def auxreduce(acc):
 inputuser = input()
 lowerin = inputuser.lower()
 splitoff = lowerin.split('off')
-splitoffon = splitoff[:1] + concat(funtakeon(splitoff))
+splitoffon = splitoff[:1] + concat(funtakeon(splitoff[1:]))
 splitoffon = concat(map(fun,splitoffon))
 print(f'Partes a considerar: {splitoffon}')
 res = reduce(lambda acc, str: auxreduce(acc) if str == '=' else acc + float(str), splitoffon, 0)
