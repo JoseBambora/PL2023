d312580570ccaf0c0378b92fcf35efc76088a0af
JoseBambora <zebragapt@gmail.com>
2023-02-24 13:20:27 +0000

    Solved homework #2.

---
+++ TPC2/exercicio.py
@@ -0,0 +1,44 @@
+from functools import reduce
+
+def fun(str):
+    res = []
+    add = ""
+    for ch in str:
+        if ch.isdigit() or ch == '.':
+            add += ch
+        else:
+            if(len(add) > 0):
+                res.append(add)
+                add = ""
+            if ch == '=':
+                res.append('=')
+    if(len(add) > 0):
+        res.append(add)
+    return res
+
+def concat(l):
+    return  [elem for lst in l for elem in lst]
+
+def funaux(l):
+    return list(filter(lambda str: str == '=',l[0])) + l[1:]
+
+def funtakeon(l):
+    return map(lambda str: funaux(str.split('on')),l[1:])
+
+def auxreduce(acc):
+    print(acc)
+    return acc
+
+inputuser = input()
+lowerin = inputuser.lower()
+splitoff = lowerin.split('off')
+splitoffon = splitoff[:1] + concat(funtakeon(splitoff))
+splitoffon = concat(map(fun,splitoffon))
+print(f'Partes a considerar: {splitoffon}')
+res = reduce(lambda acc, str: auxreduce(acc) if str == '=' else acc + float(str), splitoffon, 0)
+# for str in splitoffon:
+#     if str == '=':
+#         print(res)
+#     else:
+#         res += int(str)
+print(res)
